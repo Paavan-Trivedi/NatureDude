ec8e29a2569366e17dcf541768e00fff98161f30
darshan212116 <126803180+darshan212116@users.noreply.github.com>
2024-02-14 20:34:50 +0530

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="font">Hello NatureDude</div>
+    </>
+  );
+}
+
+export default App;
