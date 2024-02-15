15a5020f0c2d17841934c4d94577a0b06f33aa2e
007mbr <148936977+mohit-karbh@users.noreply.github.com>
2024-02-16 02:41:11 +0530

    working on slider

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -4,9 +4,9 @@ import Footer from '../Footer/Footer'
 
 const Layout = ({children}) => {
   return (
-    <div className=' flex'>
+    <div className='flex'>
         <Navbar></Navbar>
-<div className='w-[100%] px-1'>
+<div className='w-[75%] ms-[25%] px-1 '>
         {children}
       <Footer></Footer>
         </div>      
