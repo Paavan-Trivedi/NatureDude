0133d050146900a928ccb0c03f6b36ec76d70368
007mbr <148936977+mohit-karbh@users.noreply.github.com>
2024-02-15 15:11:55 +0530

    new

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Layout from '../Layout/Layout'
+
+const Home = () => {
+  return (
+   
+        <Layout>
+
+            <p>Home and aboutus</p>
+
+        </Layout>
+    
+  )
+}
+
+export default Home
