a9ed8fe9862e114dbc074b847e0dd36e436312a8
007mbr <148936977+mohit-karbh@users.noreply.github.com>
2024-02-15 18:46:40 +0530

    new component created

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+
+const Product = () => {
+  return (
+    <Layout>
+      <p>Product Category</p>
+    </Layout>
+  )
+}
+
+export default Product
